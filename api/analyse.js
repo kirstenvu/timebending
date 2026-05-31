967e3a0bc146eb9f09bc40a6bc58ccfc3110bd21
kirstenvu <kirsten@vanulden.nl>
2026-05-31 20:35:35 +0200

    Update analyse.js

--- api/analyse.js
+++ api/analyse.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS-headers altijd instellen (ook voor OPTIONS preflight)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
