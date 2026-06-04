d49a959eaeea0c58ae039554274a6c0c68affe1b
kirstenvu <kirsten@vanulden.nl>
2026-06-04 14:54:05 +0200

    Update api/feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -9,16 +9,16 @@ module.exports = async function handler(req, res) {
   const body = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
   const { naam, onderwerp, cijfer, review, beter, overig, toestemming } = body || {};
 
-  const FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSevDF6rZ6DuzncGKC2LW5gmlaBiApe74mnyf_DLBY-lLbsXHw/formResponse';
+  const FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfHli6_VF2NEpCKd2C24oQt_aJ3pL1Sr4M0dyd1qVTpy5p59g/formResponse';
 
   const params = new URLSearchParams();
-  params.append('entry.431895249', naam || '');
-  params.append('entry.946278155', onderwerp || '');
-  params.append('entry.776613450', cijfer || '');
-  params.append('entry.1468011716', review || '');
-  params.append('entry.573494669', beter || '');
-  params.append('entry.2145780264', overig || '');
-  params.append('entry.1368210254', toestemming || '');
+  params.append('entry.1877454650', naam || '');
+  params.append('entry.1930566545', onderwerp || '');
+  params.append('entry.544612005', cijfer || '');
+  params.append('entry.1892157306', review || '');
+  params.append('entry.1768337614', beter || '');
+  params.append('entry.1736839042', overig || '');
+  params.append('entry.379637694', toestemming || '');
 
   try {
     await fetch(FORM_URL, {
